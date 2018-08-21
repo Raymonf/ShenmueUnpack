a7a3710f6c5ee2da542a805d0fff5d3eb3edd394
Raymonf <raymonf@outlook.com>
2018-08-21 18:45:18 -0400

    Initial commit

---
+++ ShenmueArchiveUnpack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShenmueArchiveUnpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Path: ");
+            var path = Console.ReadLine().Replace("\"", "").Replace(".tad", "").Replace(".tac", "");
+
+            var listing = File.OpenRead(path + ".tad");
+            var data = File.OpenRead(path + ".tac");
+
+            using (var dataReader = new BinaryReader(data))
+            {
+                using (var listReader = new BinaryReader(listing))
+                {
+                    // TAD header
+                    listReader.ReadBytes(0x2C); // skip
+
+                    // Root entry
+                    listReader.ReadInt32(); // 0?
+
+                    var numFiles = listReader.ReadUInt64() - 1;
+                    listReader.ReadUInt32(); // numFiles again but 32bit?
+
+                    listReader.ReadBytes(0x10); // skip 0x10
+
+                    for (ulong i = 0; i < numFiles; i++)
+                    {
+                        var address = listReader.ReadInt64();
+                        //Console.WriteLine("Address: {0}", address);
+
+                        var size = listReader.ReadInt64();
+                        var hash = BitConverter.ToString(listReader.ReadBytes(4)).Replace("-", "").ToLower();
+                        listReader.ReadBytes(12);
+
+                        Console.WriteLine($"{i + 1}/{numFiles}");
+                        
+                        dataReader.BaseStream.Seek(address, SeekOrigin.Begin);
+                        File.WriteAllBytes(hash, dataReader.ReadBytes((int)size));
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ShenmueAudioUnpack/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShenmueAudioUnpack
+{
+    class Program
+    {
+        static uint BytesToAddress(byte[] b)
+        {
+            return (uint)(b[3] << 24 | b[2] << 16 | b[1] << 8 | b[0]);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Path: ");
+            var path = Console.ReadLine().Replace("\"", "");
+            var file = File.OpenRead(path);
+
+            var pointerList = new List<SoundPointer>();
+
+            using (var reader = new BinaryReader(file))
+            {
+                var header = reader.ReadUInt32();
+                if (header != 0x00534641)
+                {
+                    throw new Exception("Not a Shenmue AFS file.");
+                }
+
+                var numFiles = reader.ReadUInt32();
+                for (uint i = 0; i < numFiles; i++)
+                {
+                    pointerList.Add(new SoundPointer()
+                    {
+                        Address = BytesToAddress(reader.ReadBytes(4)),
+                        Size = BytesToAddress(reader.ReadBytes(4))
+                    });
+                }
+
+                for (int i = 0; i < pointerList.Count; i++)
+                {
+                    var ptr = pointerList[i];
+                    reader.BaseStream.Seek(ptr.Address, SeekOrigin.Begin);
+
+                    var bytes = reader.ReadBytes((int)ptr.Size);
+
+                    if (bytes[0] == 'R' && bytes[1] == 'I' && bytes[2] == 'F' && bytes[3] == 'F')
+                    {
+                        // Should be audio
+                        File.WriteAllBytes(i + ".xwma", bytes);
+                    }
+                    else
+                    {
+                        // Might be subtitle data
+                        File.WriteAllBytes(i + ".bin", bytes);
+                    }
+
+                    Console.WriteLine($"{i + 1}/{pointerList.Count}");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
